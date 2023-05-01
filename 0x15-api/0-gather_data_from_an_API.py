489fbdaa057c5fa0eb4362b0675c8512c8ad5715
Captressketh001 <shininglite2012@gmail.com>
2023-05-01 18:49:46 +0000

    user api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+a Python script that, using this REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    userId = argv[2]
+    url = 'https://jsonplaceholder.typicode.com/users/'
+    res = requests.get(url)
+    if res.status_code == 200:
+        users = res.json()
+        for i in users:
+            print(i)
+        
+    
