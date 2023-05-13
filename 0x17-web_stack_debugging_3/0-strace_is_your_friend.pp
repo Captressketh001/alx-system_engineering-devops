2a44f76d6d5dc326c1d6586841c23435f0405c57
Captressketh001 <shininglite2012@gmail.com>
2023-05-13 16:27:07 +0000

    fix internal server error on apache

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,6 @@
 # Fix apache 500 internal server error using strace
 
 exec {'500 error fix':
-  command   => '/bin/sed',
-  arguments => ['-i', 's/phpp/php/g', '/var/www/html/wp-settings.php'],
-  path      => '/usr/bin:/bin',
+  command => '/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/var/www/html/wp-settings.php',
 }
