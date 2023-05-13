aaaab1ab5144f005a6d139000b0ff5c8d2c18e84
Oluwakemi Omoyeni <shininglite2012@gmail.com>
2023-05-13 21:58:53 +0100

    added preliminary files and content

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a recursive function that queries the Reddit API and returns a list
+containing the titles of all hot articles for a given subreddit"""
+
+def recurse(subreddit, hot_list=[]):
+    pass
